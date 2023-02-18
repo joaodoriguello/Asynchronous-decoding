943522bda243049bf7209e42f32669696e051fc2
joaodoriguello <91113295+joaodoriguello@users.noreply.github.com>
2023-02-18 23:17:37 +0800

    Add files via upload

--- CG decoder plus degeneracy/CG decoder/CG_decoder_degeneracy_continuous.h
+++ CG decoder plus degeneracy/CG decoder/CG_decoder_degeneracy_continuous.h
@@ -32,14 +32,17 @@ using namespace std;
 // Structure to save the coordinates of a block
 typedef struct block_node {
 
-	int t0, t1, vertex;
+	double t0, t1;
+	int vertex;
 
 } block_node;
 
 // Structure to save the coordinates of an anyon block
 typedef struct syndrome_node {
 
-	int row, column, t0, t1, vertex;
+	int row, column;
+	double t0, t1;
+	int vertex;
 
 } syndrome_node;
 
@@ -59,31 +62,44 @@ bool random_error (double error) {
 /////////////////////////////////////////
 
 // Function that outputs the probability of successful decoding for a given set of inputs
-double CG_decoder_degeneracy (int L, double physical_error, double measu_error, double synchronicity, int time, int precision, double tau) {
+double CG_decoder_degeneracy_continuous (int L, double physical_error, double measu_error, int time, int precision, double tau) {
 
 	int success = 0;
 
 	int i, j, t, m;
-	int x0, y0, t0, x1, y1, t1, v;
-	bool syndrome0, syndrome1, flag;
-	double beta = log((1-measu_error)/measu_error);
+	int x0, y0, x1, y1, v;
+	double t0, t1;
+	bool syndrome0, syndrome1, flag, aux_bool;
+	double beta = log((1-physical_error)/physical_error);
 	double p;
 
-	int n_blocks, n_syn;
-	int current, block_size;
+	int n_syn, n_blocks, block_size;
+	int aux_count, current;
+	double aux_time;
 	bool error_horizontal, error_vertical;
+
+	// Lattice state after all measurement rounds
+	bool lattice_H[L][L];
+	bool lattice_V[L][L];
+
+	// List of measurement times
+	priority_queue<double, vector<double>, greater<double>> measu_list;
+	
+	// List of error times for horizontal and vertical lattices. "lattice_H_time2" will be a copy of "lattice_H_time1". The same is true for "lattice_V_time2" and "lattice_V_time1"
+	vector<vector< priority_queue<double, vector<double>, greater<double>> >> lattice_H_time1 (L, vector< priority_queue<double, vector<double>, greater<double>> >(L));
+	vector<vector< priority_queue<double, vector<double>, greater<double>> >> lattice_H_time2 (L, vector< priority_queue<double, vector<double>, greater<double>> >(L));
+	vector<vector< priority_queue<double, vector<double>, greater<double>> >> lattice_V_time1 (L, vector< priority_queue<double, vector<double>, greater<double>> >(L));
+	vector<vector< priority_queue<double, vector<double>, greater<double>> >> lattice_V_time2 (L, vector< priority_queue<double, vector<double>, greater<double>> >(L));
 
 	queue<int> queue;
 
-	bool lattice_H[L][L][time];
-	bool lattice_V[L][L][time];
+	// Poisson distributions
+	random_device rd;
+	default_random_engine generator (rd());
+	uniform_real_distribution<double> unif_distribution (0.0001,time-0.0001);
+	poisson_distribution<int> p_error (-time*0.5*log(1 - 2*physical_error));
+	poisson_distribution<int> p_measu (time);
 
-	for (i = 0; i < L; i++) {
-		for (j = 0; j < L; j++) {
-			lattice_H[i][j][0] = false;
-			lattice_V[i][j][0] = false;
-		}
-	}
 
 	for (int n = 0; n < precision; n++) {
 
@@ -91,9 +107,24 @@ double CG_decoder_degeneracy (int L, double physical_error, double measu_error, 
 
 		for (i = 0; i < L; i++) {
 			for (j = 0; j < L; j++) {
-				for (t = 1; t < time; t++) {
-					lattice_H[i][j][t] = random_error(physical_error) ^ lattice_H[i][j][t-1];
-					lattice_V[i][j][t] = random_error(physical_error) ^ lattice_V[i][j][t-1];
+
+				aux_count = p_error(generator);	// Sample number of errors from Poisson distribution
+				lattice_H[i][j] = bool(aux_count % 2);	// Lattice state at the end
+
+				for (t = 0; t < aux_count; t++) {	// Obtain the list of times of the physical errors
+					aux_time = unif_distribution(generator);
+					lattice_H_time1[i][j].push(aux_time);
+					lattice_H_time2[i][j].push(aux_time);
+				}
+
+
+				aux_count = p_error(generator);	// Sample number of errors from Poisson distribution
+				lattice_V[i][j] = bool(aux_count % 2);	// Lattice state at the end
+
+				for (t = 0; t < aux_count; t++) {	// Obtain the list of times of the physical errors
+					aux_time = unif_distribution(generator);
+					lattice_V_time1[i][j].push(aux_time);
+					lattice_V_time2[i][j].push(aux_time);
 				}
 			}
 		}
@@ -105,35 +136,81 @@ double CG_decoder_degeneracy (int L, double physical_error, double measu_error, 
 
 		vector<syndrome_node> syndrome;
 		vector<vector<vector<block_node>>> block (L, vector<vector<block_node>>(L));
-	    
+	   
 		for (i = 0; i < L; i++) {
 			for (j = 0; j < L; j++) {
 
+				aux_count = p_measu(generator);	// Sample number of errors from Poisson distribution
+				for (t = 0; t < aux_count; t++)
+					measu_list.push(unif_distribution(generator));
+
 				syndrome0 = false;
-				t0 = 0;
+				aux_bool = false;
+				t0 = 0.0;
 
-				for (t = 1; t < time - 1; t++) {
-					if (random_error(synchronicity)) {	// Check if stabilizer measurement is successful
+				while (!measu_list.empty()) {
 
-						block[i][j].push_back(block_node({t0,t,n_blocks}));	// All blocks are numbered by their order of appearance = n_blocks
+					t1 = measu_list.top();
+					measu_list.pop();
 
-						syndrome1 = lattice_H[i][j][t] ^ lattice_H[i][(j-1+L)%L][t] ^ lattice_V[i][j][t] ^ lattice_V[(i-1+L)%L][j][t] ^ random_error(measu_error);
-						if (syndrome0 != syndrome1)	// Different consecutive measurements lead to an anyon block
-							syndrome.push_back(syndrome_node({i,j,t0,t,n_blocks}));
-					
-						t0 = t;
-						syndrome0 = syndrome1;
-						n_blocks++;
+					block[i][j].push_back(block_node({t0,t1,n_blocks}));	// All blocks are numbered by their order of appearance = n_blocks
+		
+					// Check the parity of the number of physical errors prior to the measurement time
+					while (!lattice_H_time1[i][j].empty() and lattice_H_time1[i][j].top() <= t1) {
+						aux_bool = !aux_bool;
+						lattice_H_time1[i][j].pop();
 					}
+
+					while (!lattice_V_time1[i][j].empty() and lattice_V_time1[i][j].top() <= t1) {
+						aux_bool = !aux_bool;
+						lattice_V_time1[i][j].pop();
+					}
+
+					while (!lattice_H_time2[i][(j-1+L)%L].empty() and lattice_H_time2[i][(j-1+L)%L].top() <= t1) {
+						aux_bool = !aux_bool;
+						lattice_H_time2[i][(j-1+L)%L].pop();
+					}
+
+					while (!lattice_V_time2[(i-1+L)%L][j].empty() and lattice_V_time2[(i-1+L)%L][j].top() <= t1) {
+						aux_bool = !aux_bool;
+						lattice_V_time2[(i-1+L)%L][j].pop();
+					}
+
+					syndrome1 = aux_bool ^ random_error(physical_error);	// Add measurement error
+					if (syndrome0 != syndrome1)	// Different consecutive measurements lead to an anyon block
+						syndrome.push_back(syndrome_node({i,j,t0,t1,n_blocks}));
+				
+					t0 = t1;
+					syndrome0 = syndrome1;
+					n_blocks++;
 				}
 
-				block[i][j].push_back(block_node({t0,time-1,n_blocks}));	// All blocks are numbered by their order of appearance = n_blocks
+				block[i][j].push_back(block_node({t0,double(time),n_blocks}));	// All blocks are numbered by their order of appearance = n_blocks
 
 				// Last measurement round with perfect stabilizer measurement
-				syndrome1 = lattice_H[i][j][time-1] ^ lattice_H[i][(j-1+L)%L][time-1] ^ lattice_V[i][j][time-1] ^ lattice_V[(i-1+L)%L][j][time-1];
-				if (syndrome0 != syndrome1) 	// Different consecutive measurements lead to an anyon block
-					syndrome.push_back(syndrome_node({i,j,t0,time-1,n_blocks}));
-			
+				while (!lattice_H_time1[i][j].empty() and lattice_H_time1[i][j].top() <= time) {
+					aux_bool = !aux_bool;
+					lattice_H_time1[i][j].pop();
+				}
+
+				while (!lattice_V_time1[i][j].empty() and lattice_V_time1[i][j].top() <= time) {
+					aux_bool = !aux_bool;
+					lattice_V_time1[i][j].pop();
+				}
+
+				while (!lattice_H_time2[i][(j-1+L)%L].empty() and lattice_H_time2[i][(j-1+L)%L].top() <= time) {
+					aux_bool = !aux_bool;
+					lattice_H_time2[i][(j-1+L)%L].pop();
+				}
+
+				while (!lattice_V_time2[(i-1+L)%L][j].empty() and lattice_V_time2[(i-1+L)%L][j].top() <= time) {
+					aux_bool = !aux_bool;
+					lattice_V_time2[(i-1+L)%L][j].pop();
+				}
+
+				if (syndrome0 != aux_bool)	// Different consecutive measurements lead to an anyon block
+					syndrome.push_back(syndrome_node({i,j,t0,double(time),n_blocks}));
+
 				n_blocks++;
 			}
 	   	}
@@ -153,13 +230,13 @@ double CG_decoder_degeneracy (int L, double physical_error, double measu_error, 
 
 
 				for (m = 0; block[(i+1+L)%L][j][m].t1 <= t0; m++);	// Find the first adjacent block overlapping in time with the current block
-
-				// Connect all adjacent blocks overlapping in time with the current block		
+			
+				// Connect all adjacent blocks overlapping in time with the current block
 				while (m < block[(i+1+L)%L][j].size() and block[(i+1+L)%L][j][m].t0 < t1) {
 					block_graph[v].insert(block[(i+1+L)%L][j][m].vertex);
 					block_graph[block[(i+1+L)%L][j][m].vertex].insert(v);
 
-					p = synchronicity*(MIN(block[(i+1+L)%L][j][m].t1,t1) - MAX(block[(i+1+L)%L][j][m].t0,t0));	// Adjacent-blocks time overlap
+					p = MIN(block[(i+1+L)%L][j][m].t1,t1) - MAX(block[(i+1+L)%L][j][m].t0,t0);	// Adjacent-blocks time overlap
 					degeneracy_graph[v][block[(i+1+L)%L][j][m].vertex] = p;
 					degeneracy_graph[block[(i+1+L)%L][j][m].vertex][v] = p;
 
@@ -174,7 +251,7 @@ double CG_decoder_degeneracy (int L, double physical_error, double measu_error, 
 					block_graph[v].insert(block[i][(j+1+L)%L][m].vertex);
 					block_graph[block[i][(j+1+L)%L][m].vertex].insert(v);
 
-					p = synchronicity*(MIN(block[i][(j+1+L)%L][m].t1,t1) - MAX(block[i][(j+1+L)%L][m].t0,t0));	// Adjacent-blocks time overlap
+					p = MIN(block[i][(j+1+L)%L][m].t1,t1) - MAX(block[i][(j+1+L)%L][m].t0,t0);	// Adjacent-blocks time overlap
 					degeneracy_graph[v][block[i][(j+1+L)%L][m].vertex] = p;
 					degeneracy_graph[block[i][(j+1+L)%L][m].vertex][v] = p;
 
@@ -190,13 +267,13 @@ double CG_decoder_degeneracy (int L, double physical_error, double measu_error, 
 
 
 					for (m = 0; block[(i+1+L)%L][j][m].t1 <= t0; m++);	// Find the first adjacent block overlapping in time with the current block
-				
+	
 					// Connect all adjacent blocks overlapping in time with the current block
 					while (m < block[(i+1+L)%L][j].size() and block[(i+1+L)%L][j][m].t0 < t1) {
 						block_graph[v].insert(block[(i+1+L)%L][j][m].vertex);
 						block_graph[block[(i+1+L)%L][j][m].vertex].insert(v);
 
-						p = synchronicity*(MIN(block[(i+1+L)%L][j][m].t1,t1) - MAX(block[(i+1+L)%L][j][m].t0,t0));	// Adjacent-blocks time overlap
+						p = MIN(block[(i+1+L)%L][j][m].t1,t1) - MAX(block[(i+1+L)%L][j][m].t0,t0);	// Adjacent-blocks time overlap
 						degeneracy_graph[v][block[(i+1+L)%L][j][m].vertex] = p;
 						degeneracy_graph[block[(i+1+L)%L][j][m].vertex][v] = p;
 
@@ -206,12 +283,12 @@ double CG_decoder_degeneracy (int L, double physical_error, double measu_error, 
 
 					for (m = 0; block[i][(j+1+L)%L][m].t1 <= t0; m++);	// Find the first adjacent block overlapping in time with the current block
 	
-					// Connect all adjacent blocks overlapping in time with the current block	
+					// Connect all adjacent blocks overlapping in time with the current block
 					while (m < block[i][(j+1+L)%L].size() and block[i][(j+1+L)%L][m].t0 < t1) {
 						block_graph[v].insert(block[i][(j+1+L)%L][m].vertex);
 						block_graph[block[i][(j+1+L)%L][m].vertex].insert(v);
 
-						p = synchronicity*(MIN(block[i][(j+1+L)%L][m].t1,t1) - MAX(block[i][(j+1+L)%L][m].t0,t0));	// Adjacent-blocks time overlap
+						p = MIN(block[i][(j+1+L)%L][m].t1,t1) - MAX(block[i][(j+1+L)%L][m].t0,t0);	// Adjacent-blocks time overlap
 						degeneracy_graph[v][block[i][(j+1+L)%L][m].vertex] = p;
 						degeneracy_graph[block[i][(j+1+L)%L][m].vertex][v] = p;
 
@@ -271,8 +348,8 @@ double CG_decoder_degeneracy (int L, double physical_error, double measu_error, 
 
 	    	pm->Solve();	// Use Edmonds' matching algorithm
 
-	    	for (i = 0; i < n_syn; i++) {
-			j = pm->GetMatch(i);	// Get the anyon matched with i
+	    	for (i = 0; i < n_syn; i++) {	// Get the anyon matched with i
+			j = pm->GetMatch(i);
 
 			if (i < j) {
 		    		x0 = syndrome[i].row;
@@ -283,20 +360,20 @@ double CG_decoder_degeneracy (int L, double physical_error, double measu_error, 
 				// Flip the lattice edges between anyons i and j
 		    		if ((x1-x0+L) % L < (x0-x1+L) % L) {
 					for (m = 0; m < (x1-x0+L) % L; m++)
-			    			lattice_V[(x1-m-1+L)%L][y1][time-1] ^= true;
+			    			lattice_V[(x1-m-1+L)%L][y1] ^= true;
 		    		}
 		    		else {
 					for (m = 0; m < (x0-x1+L) % L; m++)
-			    			lattice_V[(x1+m+L)%L][y1][time-1] ^= true;
+			    			lattice_V[(x1+m+L)%L][y1] ^= true;
 		    		}
 
 		    		if ((y1-y0+L) % L < (y0-y1+L) % L) {
 					for (m = 0; m < (y1-y0+L) % L; m++)
-			    			lattice_H[x0][(y1-m-1+L)%L][time-1] ^= true;
+			    			lattice_H[x0][(y1-m-1+L)%L] ^= true;
 		    		}
 		    		else {
 					for (m = 0; m < (y0-y1+L) % L; m++)
-			    			lattice_H[x0][(y1+m+L)%L][time-1] ^= true;
+			    			lattice_H[x0][(y1+m+L)%L] ^= true;
 		    		}
 			}
 	    	}
@@ -312,8 +389,8 @@ double CG_decoder_degeneracy (int L, double physical_error, double measu_error, 
 
 			// Check for the parity of the number of errors along any column or row
 			for (j = 0; j < L; j++) {
-		    		error_horizontal ^= lattice_H[j][i][time-1];
-		    		error_vertical ^= lattice_V[i][j][time-1];
+		    		error_horizontal ^= lattice_H[j][i];
+		    		error_vertical ^= lattice_V[i][j];
 			}
 			// Check if there is a logical error
 			if (error_horizontal or error_vertical) {
@@ -321,7 +398,7 @@ double CG_decoder_degeneracy (int L, double physical_error, double measu_error, 
 				break;
 			}
 		}
-	    	if (flag)
+		if (flag)
 			success++;
 	}
 
